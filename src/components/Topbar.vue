9a6bb4b88cea7798108f33dc6e2b4cc90978e7fd
m-yahya <yahya_gis@hotmail.com>
2019-05-10 18:16:52 +0200

    Add Project Starter Code

---
+++ src/components/Topbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <h1>Oli Energey Geo Browser</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Topbar"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
