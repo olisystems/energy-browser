3d2ce8f30037315afb42a5422fa5fa449e74956b
m-yahya <yahya_gis@hotmail.com>
2019-05-30 17:58:49 +0200

    Update block mining time

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -68,7 +68,7 @@ export default {
           this.latestBlock = blockNumber;
           $(".block-loader").hide();
         });
-      }, 3000);
+      }, 60000);
     },
     getTransactions() {
       setInterval(() => {
@@ -76,7 +76,7 @@ export default {
           this.transactions = count;
           $(".tx-loader").hide();
         });
-      }, 3000);
+      }, 60000);
     },
     getGasPrice() {
       setInterval(() => {
@@ -84,7 +84,7 @@ export default {
           this.gas = (price / 1000000).toFixed(2);
           $(".gas-loader").hide();
         });
-      }, 3000);
+      }, 60000);
     },
     callFunc() {
       this.getProducers();
