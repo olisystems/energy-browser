bcb290496655b5f04e6f6e47c4f8572ac492d263
m-yahya <yahya_gis@hotmail.com>
2019-06-14 12:31:58 +0200

    Config: vue-router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './routes';
 
 import SmartTable from 'vuejs-smart-table'
 Vue.use(SmartTable)
+Vue.use(VueRouter)
+const router = new VueRouter({ routes });
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
