3e8004f461087e56aa5d95df95063f3811646977
m-yahya <yahya_gis@hotmail.com>
2019-06-15 02:37:08 +0200

    Add app routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,11 @@
-const routes = [
+import Browser from './components/Browser.vue';
+import Explorer from './components/Explorer.vue';
+import Tokenization from './components/Tokenization.vue';
 
+const routes = [
+    { path: '/', component: Browser },
+    { path: '/explorer', component: Explorer },
+    { path: '/tokenization', component: Tokenization }
 ];
 
 export default routes;
