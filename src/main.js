d1880bc6c843ecdc9b817b8013f3efeeb85e6181
m-yahya <yahya_gis@hotmail.com>
2019-06-15 02:38:32 +0200

    Add font awesome

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import SmartTable from 'vuejs-smart-table'
 import VueRouter from 'vue-router'
 import routes from './routes';
 
-import SmartTable from 'vuejs-smart-table'
+import './assets/css/main.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faFacebook, faTwitter, faGithub)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.use(SmartTable)
 Vue.use(VueRouter)
 const router = new VueRouter({ routes });
