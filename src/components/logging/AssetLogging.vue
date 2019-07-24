23b71c309a4f371e90e12b93339b2a96e509927b
m-yahya <yahya_gis@hotmail.com>
2019-07-24 11:34:13 +0200

    Change body width

--- src/components/logging/AssetLogging.vue
+++ src/components/logging/AssetLogging.vue
@@ -18,8 +18,15 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style >
 .not-found {
   color: #bbb;
 }
+
+#asset-logging {
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
+
+
