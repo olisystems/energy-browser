39230d3fb8e71fa0aed7d57581cb083b61ebe997
m-yahya <yahya_gis@hotmail.com>
2020-02-25 13:01:14 +0100

    Add routes to charging origin

--- app/src/routes.js
+++ app/src/routes.js
@@ -2,12 +2,14 @@ import Browser from './components/browser/Browser.vue';
 import Explorer from './components/explorer/Explorer.vue';
 import Tokenization from './components/tokenization/Tokenization.vue';
 import AssetLogging from './components/logging/AssetLogging.vue';
+import ChargingOrigin from './components/origin/ChargingOrigin.vue';
 
 const routes = [
     { path: '/', component: Browser },
     { path: '/explorer', component: Explorer },
     { path: '/tokenization', component: Tokenization },
-    { path: '/logging', component: AssetLogging }
+    { path: '/logging', component: AssetLogging },
+    { path: '/origin', component: ChargingOrigin }
 ];
 
 export default routes;
