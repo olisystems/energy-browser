f55eead9022010ed4326c5f7b21aa7083e83599b
m-yahya <yahya_gis@hotmail.com>
2020-12-11 14:33:56 +0100

    Update WS to WSS

--- app/src/main.js
+++ app/src/main.js
@@ -17,7 +17,8 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(SmartTable)
 Vue.use(VTooltip)
 Vue.use(VueRouter)
-Vue.use(Web3Plugin, { url: "ws://80.158.47.134:7546" })
+Vue.use(Web3Plugin, { url: "wss://services.my-oli.com/volta-node" })
+
 
 import 'v-tooltip/dist/v-tooltip.css'
 
