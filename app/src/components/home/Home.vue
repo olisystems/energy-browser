fb1a0c5acc7cec9d0018b22ed798447c7a85e73f
m-yahya <yahya_gis@hotmail.com>
2021-01-02 14:19:13 +0100

    Add home page component

---
+++ app/src/components/home/Home.vue
@@ -0,0 +1,299 @@
+<template>
+  <div class="home-page">
+    <div class="header">
+      <div class="header__heading-box">
+        <h1 class="heading-primary">
+          <span class="heading-primary--main">OLI DAPPs</span>
+          <span class="heading-primary--sub">Ethereum Based Dapps</span>
+        </h1>
+
+        <a href="#" class="btn btn--white btn--animated">Discover Dapps</a>
+      </div>
+    </div>
+
+    <main>
+      <section class="section-about">
+        <div class="u-center-text u-margin-bottom-big">
+          <h2 class="heading-secondary">
+            Decentralized Apps on the top of Ethereum Network
+          </h2>
+        </div>
+
+        <div class="row">
+          <div class="col-1-of-2">
+            <h3 class="heading-tertiary u-margin-bottom-small">
+              A collection of Decentralized applications
+            </h3>
+            <p class="paragraph">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo
+              saepe magni ipsa, deserunt, iste autem harum neque repellendus,
+              perferendis illo excepturi rem eius delectus laudantium iure
+              recusandae! Tenetur, voluptatibus excepturi?
+            </p>
+
+            <h3 class="heading-tertiary u-margin-bottom-small">
+              A collection of Decentralized applications 2
+            </h3>
+            <p class="paragraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis nemo, facere molestiae voluptatum ipsam excepturi
+              exercitationem facilis ea architecto dolorum adipisci ipsum
+              consectetur soluta atque deleniti quidem delectus magni corporis.
+            </p>
+
+            <a href="#" class="btn-text">Learn more &rarr;</a>
+          </div>
+
+          <div class="col-1-of-2">
+            <div class="composition">
+              <img
+                src="img/browser.png"
+                alt="OLI Energy Browser App"
+                class="composition__photo composition__photo--p1"
+              />
+              <img
+                src="img/explorer.png"
+                alt="OLI Tokenization App"
+                class="composition__photo composition__photo--p2"
+              />
+              <img
+                src="img/charging-origin.png"
+                alt="OLI Charging Origin App"
+                class="composition__photo composition__photo--p3"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-features">
+        <div class="row">
+          <div class="col-1-of-4">
+            <div class="feature-box">
+              <h3 class="heading-tertiary u-margin-bottom-small">
+                Volta Test Network
+              </h3>
+              <p class="feature-box__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Suscipit maxime, dignissimos rem soluta enim numquam impedit non
+                labore reprehenderit aut tenetur harum ab distinctio consectetur
+                deleniti, explicabo, ipsum ipsam quasi.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-1-of-4">
+            <div class="feature-box">
+              <h3 class="heading-tertiary u-margin-bottom-small">
+                Smart Contracts
+              </h3>
+              <p class="feature-box__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Suscipit maxime, dignissimos rem soluta enim numquam impedit non
+                labore reprehenderit aut tenetur harum ab distinctio consectetur
+                deleniti, explicabo, ipsum ipsam quasi.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-1-of-4">
+            <div class="feature-box">
+              <h3 class="heading-tertiary u-margin-bottom-small">Web3 js</h3>
+              <p class="feature-box__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Suscipit maxime, dignissimos rem soluta enim numquam impedit non
+                labore reprehenderit aut tenetur harum ab distinctio consectetur
+                deleniti, explicabo, ipsum ipsam quasi.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-1-of-4">
+            <div class="feature-box">
+              <h3 class="heading-tertiary u-margin-bottom-small">Vue js</h3>
+              <p class="feature-box__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Suscipit maxime, dignissimos rem soluta enim numquam impedit non
+                labore reprehenderit aut tenetur harum ab distinctio consectetur
+                deleniti, explicabo, ipsum ipsam quasi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-dapps">
+        <div class="u-center-text u-margin-bottom-big">
+          <h2 class="heading-secondary">Most Exciting Dapps</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-1-of-5">
+            <div class="card">
+              <div class="card__side card__side--front">
+                <div class="card__picture card__picture--1">&nbsp;</div>
+                <h4 class="card__heading">
+                  <span class="card__heading-span card__heading-span--1">
+                    Charging Origin</span
+                  >
+                </h4>
+                <div class="card__details">
+                  <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                    <li>item 5</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card__side card__side--back card__side--back-1">
+                <div class="card__cta">
+                  <div class="card__app-box">
+                    <p class="card__app-prefix">OLI</p>
+                    <p class="card__app-name">Charging Origin</p>
+                  </div>
+                  <a href="#" class="btn btn--white">Open</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-1-of-5">
+            <div class="card">
+              <div class="card__side card__side--front">
+                <div class="card__picture card__picture--2">&nbsp;</div>
+                <h4 class="card__heading">
+                  <span class="card__heading-span card__heading-span--2">
+                    Chain Explorer</span
+                  >
+                </h4>
+                <div class="card__details">
+                  <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                    <li>item 5</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card__side card__side--back card__side--back-2">
+                <div class="card__cta">
+                  <div class="card__app-box">
+                    <p class="card__app-prefix">OLI</p>
+                    <p class="card__app-name">Chain Explorer</p>
+                  </div>
+                  <a href="#" class="btn btn--white">Open</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-1-of-5">
+            <div class="card">
+              <div class="card__side card__side--front">
+                <div class="card__picture card__picture--3">&nbsp;</div>
+                <h4 class="card__heading">
+                  <span class="card__heading-span card__heading-span--3">
+                    Energy Browser</span
+                  >
+                </h4>
+                <div class="card__details">
+                  <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                    <li>item 5</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card__side card__side--back card__side--back-3">
+                <div class="card__cta">
+                  <div class="card__app-box">
+                    <p class="card__app-prefix">OLI</p>
+                    <p class="card__app-name">Energy Browser</p>
+                  </div>
+                  <a href="#" class="btn btn--white">Open</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-1-of-5">
+            <div class="card">
+              <div class="card__side card__side--front">
+                <div class="card__picture card__picture--4">&nbsp;</div>
+                <h4 class="card__heading">
+                  <span class="card__heading-span card__heading-span--4">
+                    OLI Tokenization</span
+                  >
+                </h4>
+                <div class="card__details">
+                  <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                    <li>item 5</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card__side card__side--back card__side--back-4">
+                <div class="card__cta">
+                  <div class="card__app-box">
+                    <p class="card__app-prefix">OLI</p>
+                    <p class="card__app-name">Tokenization</p>
+                  </div>
+                  <a href="#" class="btn btn--white">Open</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-1-of-5">
+            <div class="card">
+              <div class="card__side card__side--front">
+                <div class="card__picture card__picture--5">&nbsp;</div>
+                <h4 class="card__heading">
+                  <span class="card__heading-span card__heading-span--5">
+                    Asset Logging</span
+                  >
+                </h4>
+                <div class="card__details">
+                  <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                    <li>item 4</li>
+                    <li>item 5</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="card__side card__side--back card__side--back-5">
+                <div class="card__cta">
+                  <div class="card__app-box">
+                    <p class="card__app-prefix">OLI</p>
+                    <p class="card__app-name">Asset Logging</p>
+                  </div>
+                  <a href="#" class="btn btn--white">Open</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
