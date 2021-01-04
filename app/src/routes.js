44d5a88931f42bb9310faf9e993c9e29408905d8
m-yahya <yahya_gis@hotmail.com>
2021-01-04 16:09:55 +0100

    Add route for home page

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,3 +1,4 @@
+import Home from './components/home/Home.vue';
 import Browser from './components/browser/Browser.vue';
 import Explorer from './components/explorer/Explorer.vue';
 import Tokenization from './components/tokenization/Tokenization.vue';
@@ -5,7 +6,8 @@ import AssetLogging from './components/logging/AssetLogging.vue';
 import ChargingOrigin from './components/origin/ChargingOrigin.vue';
 
 const routes = [
-    { path: '/', component: Browser },
+    { path: '/', component: Home },
+    { path: '/browser', component: Browser },
     { path: '/explorer', component: Explorer },
     { path: '/tokenization', component: Tokenization },
     { path: '/logging', component: AssetLogging },
