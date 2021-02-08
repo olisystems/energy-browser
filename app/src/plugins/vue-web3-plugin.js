ff28805dbb234f10d2d9d7b067666569b49d0479
m-yahya <yahya_gis@hotmail.com>
2021-02-08 12:53:40 +0100

    Update web3 provider setup

--- app/src/plugins/vue-web3-plugin.js
+++ app/src/plugins/vue-web3-plugin.js
@@ -1,27 +1,24 @@
+/* eslint-disable */
 import Web3 from "web3";
 
 export default {
+  install(Vue, options) {
+    (async function () {
+      const getProvider = () => {
+        const provider = new Web3.providers.WebsocketProvider(options.url);
+        provider.on("connect", () => console.log("WS Connected"));
+        provider.on("error", (e) => {
+          console.error("WS Error", e);
+          provider.setProvider(getProvider());
+        });
+        provider.on("end", (e) => {
+          console.error("WS End", e);
+          provider.setProvider(getProvider());
+        });
 
-    install(Vue, options) {
-
-        (async function () {
-            const getProvider = () => {
-                const provider = new Web3.providers.WebsocketProvider(options.url)
-                provider.on('connect', () => console.log('WS Connected'))
-                provider.on('error', e => {
-                    console.error('WS Error', e)
-                    web3.setProvider(getProvider())
-                })
-                provider.on('end', e => {
-                    console.error('WS End', e)
-                    web3.setProvider(getProvider())
-                })
-
-                return provider
-            }
-            window.web3 = new Web3(getProvider())
-
-        }())
-
-    }
-}
+        return provider;
+      };
+      window.web3 = new Web3(getProvider());
+    })();
+  },
+};
