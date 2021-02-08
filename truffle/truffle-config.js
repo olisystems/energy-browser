29b7e6c7f55f98f5449f396bbc2f889217dcb59b
m-yahya <yahya_gis@hotmail.com>
2021-02-09 00:58:03 +0100

    Clear comments

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,100 +1,39 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+const fs = require("fs");
+const path = require("path");
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-const fs = require('fs');
-
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const path = require('path');
+const ENDPOINT = "ws://services.my-oli.com/volta-node";
 const MNEMONIC = fs.readFileSync(".secret").toString().trim();
-var ENDPOINT = "ws://services.my-oli.com/volta-node";
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
-  contracts_build_directory: path.join(__dirname, '../app/src/assets/js/contracts'),
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+  contracts_build_directory: path.join(
+    __dirname,
+    "../app/src/assets/js/contracts"
+  ),
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8546,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      websockets: true
+      host: "127.0.0.1",
+      port: 8546,
+      network_id: "*",
+      websockets: true,
     },
+
     voltaRPC: {
       provider: function () {
-        return new HDWalletProvider(MNEMONIC, ENDPOINT)
+        return new HDWalletProvider(MNEMONIC, ENDPOINT);
       },
-      network_id: 73799
+      network_id: 73799,
     },
+
     volta: {
       host: "80.158.47.134",
       port: 7546,
       network_id: 73799,
       websockets: true,
     },
-
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
-  // Set default mocha options here, use special reporters etc.
   mocha: {
     enableTimeouts: false,
   },
@@ -111,7 +50,7 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
-  }
-}
+    },
+  },
+};
 // "PV 7", "PV", "0x3D481ee06aFe587dAe5EAFA541c75c3D1F9dCdBc", "0xe2e457aB987BEd9AbdEE9410FC985E46e28a3947", "0x157cb51153D26B3F4491a3e706d005d0ac696376", 400, 800, "Urban", "25-11-2019"
