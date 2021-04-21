76f89254286b3627ca54d4e83f34df52ecfff756
m-yahya <yahya_gis@hotmail.com>
2021-04-21 16:49:38 +0200

    Apply code formatting

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,17 +1,17 @@
-import Home from './components/home/Home.vue';
-import Browser from './components/browser/Browser.vue';
-import Explorer from './components/explorer/Explorer.vue';
-import Tokenization from './components/tokenization/Tokenization.vue';
-import AssetLogging from './components/logging/AssetLogging.vue';
-import ChargingOrigin from './components/origin/ChargingOrigin.vue';
+import Home from "./components/home/Home.vue";
+import Browser from "./components/browser/Browser.vue";
+import Explorer from "./components/explorer/Explorer.vue";
+import Tokenization from "./components/tokenization/Tokenization.vue";
+import AssetLogging from "./components/logging/AssetLogging.vue";
+import ChargingOrigin from "./components/origin/ChargingOrigin.vue";
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/browser', component: Browser },
-    { path: '/explorer', component: Explorer },
-    { path: '/tokenization', component: Tokenization },
-    { path: '/logging', component: AssetLogging },
-    { path: '/origin', component: ChargingOrigin }
+  { path: "/", component: Home },
+  { path: "/browser", component: Browser },
+  { path: "/explorer", component: Explorer },
+  { path: "/tokenization", component: Tokenization },
+  { path: "/logging", component: AssetLogging },
+  { path: "/origin", component: ChargingOrigin },
 ];
 
 export default routes;
