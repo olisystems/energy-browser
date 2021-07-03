8bb397b8ca89258b348db8adc7f39e849db18eb4
m-yahya <yahya_gis@hotmail.com>
2021-07-03 12:49:46 +0200

    Change URL to IP

--- app/src/assets/js/web3.js
+++ app/src/assets/js/web3.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import Web3 from "web3";
 
-const VOLTA_NODE = "wss://volta-blockchain.my-oli.com";
+const VOLTA_NODE = "wss://80.158.47.134";
 //var provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
 // var web3 = new Web3("ws://80.158.47.134:7546");
 
