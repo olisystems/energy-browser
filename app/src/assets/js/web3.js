67351aa945782b0c849cad9b7440411f61606c51
Muhammad Yahya <yahya_gis@hotmail.com>
2021-07-05 22:47:16 +0200

    Remove upstream block

--- app/src/assets/js/web3.js
+++ app/src/assets/js/web3.js
@@ -2,7 +2,7 @@
 import Web3 from "web3";
 
  //const VOLTA_NODE = "wss://volta-blockchain.my-oli.com";
-const VOLTA_NODE = "ws://80.158.47.134:7546";
+const VOLTA_NODE = "wss://80.158.47.134";
 //var provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
 // var web3 = new Web3("ws://80.158.47.134:7546");
 
