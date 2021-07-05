0e2ec5bff4b4684ba7ced0ef872f3545ac28ed8a
Muhammad Yahya <yahya_gis@hotmail.com>
2021-07-05 22:28:58 +0200

    Update url

--- app/src/assets/js/web3.js
+++ app/src/assets/js/web3.js
@@ -1,38 +1,12 @@
 /* eslint-disable */
 import Web3 from "web3";
 
-// const VOLTA_NODE = "wss://volta-blockchain.my-oli.com";
-const VOLTA_NODE = "wss://80.158.47.134";
+ //const VOLTA_NODE = "wss://volta-blockchain.my-oli.com";
+const VOLTA_NODE = "ws://80.158.47.134:7546";
 //var provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
 // var web3 = new Web3("ws://80.158.47.134:7546");
 
  var web3 = new Web3(VOLTA_NODE);
 
-// // on connect
-// provider.on("connect", () => console.log("WS Connected"));
-
-// // on error
-// provider.on("error", () => {
-//   console.log("WS Error");
-//   console.log("Attempting to reconnect...");
-//   provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
-
-//   provider.on("connect", () => console.log("WS Re-connected"));
-
-//   web3.setProvider(provider);
-//   location.reload();
-// });
-
-// // on connection close
-// provider.on("end", () => {
-//   console.log("WS closed");
-//   console.log("Attempting to reconnect...");
-//   provider = new Web3.providers.WebsocketProvider(VOLTA_NODE);
-
-//   provider.on("connect", () => console.log("WS Re-connected"));
-
-//   web3.setProvider(provider);
-//   location.reload();
-//});
 
 export default web3;
